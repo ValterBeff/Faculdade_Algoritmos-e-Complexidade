b895641da8c6d6e803c81947956bbe1802ea5db2
Valter Beff <130705874+ValterBeff@users.noreply.github.com>
2025-02-18 21:08:56 -0300

    Add Aula001

---
+++ Aula001.cpp
@@ -0,0 +1,21 @@
+/*
+Aula 001 - Estrutura de um Programa em C++
+*/
+
+//Código base em C++
+#include <iostream> // Biblioteca de Funções
+using namespace std; // Funções Standard
+
+//Função Proncipal
+int main()
+{
+	// Variáveis (declare aqui os tipos e as variáveis a usar)
+	
+	// entradas de dados (comendos atribuindo ou solicitando dados)
+	
+	// processamento de dados (cálculos, testes, verificações, etc.)
+	
+	// saida de dados (exibição dos resultados obtidos pelo processamento)
+	
+	return 0;
+} //fim do main()
